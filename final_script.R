52a26003970519cc0a7013e84edf52fed8e0dff8
kmm0155 <60485331+kmm0155@users.noreply.github.com>
2020-04-14 11:50:22 -0500

    Update final_script.R

--- final_script.R
+++ final_script.R
@@ -1,7 +1,11 @@
 #This page will describe an efficient and useful way to create manuscript-ready graphs using GGplot.
 #The below script would be run in R/R Studio.
 
-#Library#
+#Set your working directory for where files will save. You will need to insert your own preferred directory.
+setwd("/Users/kaitlynmurphy/Desktop/JD")
+getwd()
+
+#Call on the libraries we will be using in this script.
 
 library(nlme)
 library(lme4)
@@ -10,44 +14,36 @@ library(emmeans)
 library(grid)
 library(gridExtra)
 
-#Data Loaded#
+#Load your data (in CSV format) and check to make sure R uploaded it correctly.
 
 datum=read.csv(file.choose())
 head(datum)
 
-setwd("/Users/kaitlynmurphy/Desktop/JD")
-getwd()
-
-#plot- body condition
+#Begin by running a basic linear mixed-effects model and call on the summary to see the results.
   
-  resultshatchmass2=lme(log(MASSBEF)~log(SVL),data=datum,random=~1|MOM,na.action=na.omit)
+resultshatchmass2=lme(log(MASSBEF)~log(SVL),data=datum,random=~1|MOM,na.action=na.omit)
+summary(resultshatchmass2)
 
+#Next, we need to convert these values into residuals that we can plot. Do this first by creating a new dataset (datum4)
 datum4 <- datum[complete.cases(datum[,"MASSBEF"]),]
 
-  datum4$resids <- resid(resultshatchmass2)
-  
-  boxplot(resids~YOLKREM,datum4)
-  plot(resids~EMASSBEF,datum4)
-  
-rem <- datum4[datum4$YOLKREM=="YES",]
-summary(rem)
-Nrem <- datum4[datum4$YOLKREM=="NO",]
-summary(Nrem)
+#Next, add a column wihtin the dataset that is the residuals of the linear mixed-effects model. 
+datum4$resids <- resid(resultshatchmass2)
 
-#how to insert an annotation
+#Check out a very basic boxplot of these data.
+plot(resids~EMASSBEF,datum4)
 
-annotate(geom="text",x=0.95,
-         y=0.05,label="Treatment", color="black") +
+#Now let's graph this with GGPLOT! 
 
-#body condition graph
+#First call on the tiff function that will save this figure as a tiff file on your desktop.
   
-  tiff("BodyCondition.tiff", width = 8, height = 6, units = 'in', res = 300)
+tiff("BodyCondition.tiff", width = 8, height = 6, units = 'in', res = 300)
+
+#Now to create the varibale that will be section (A) in our figure.
 
 bodycondition <- ggplot() +
-    geom_point(data=rem, aes(x=EMASSBEF, y=resids), shape=1) +
-    geom_point(data=Nrem, aes(x=EMASSBEF, y=resids)) +
-    geom_smooth(data=rem, aes(x=EMASSBEF, y=resids), method=lm,se=FALSE, color="black", linetype="dashed") +
-    geom_smooth(data=Nrem, aes(x=EMASSBEF, y=resids), method=lm,se=FALSE, color="black") +
+    geom_point(data=datum4, aes(x=EMASSBEF, y=resids), shape=1) +
+    geom_smooth(data=datum4, aes(x=EMASSBEF, y=resids), method=lm,se=FALSE, color="black") +
     theme_classic() +
     labs(title="", x="Egg mass (g) at oviposition", y="Hatchling body condition") +
     scale_x_continuous(breaks=seq(0.85,1.25,0.05), limits=c(0.85,1.25)) +
@@ -55,13 +51,12 @@ bodycondition <- ggplot() +
           axis.text.x  = element_text(size=8,color="black"),
           axis.title.y = element_text(size=10,vjust=3),
           axis.text.y  = element_text(size=8,color="black"))
-  
+
+#Turn 'off' the tiff function with dev.off
+
 dev.off()
 
-  #hatchling mass graph
-  
-  rem1 <- datum[datum$YOLKREM=="YES",]
-  Nrem1 <- datum[datum$YOLKREM=="NO",]
+#Now, let's create section (B) for our figure! Again, begin by calling on the tiff function
   
   tiff("Hatchlingmass.tiff", width = 8, height = 6, units = 'in', res = 300)
   
@@ -118,3 +113,8 @@ hatchlingsvl <- ggplot() +
   grid.arrange(hatchlingmass1, hatchlingsvl1, bodycondition1, ncol = 1)
   
   dev.off()
+
+#how to insert an annotation
+
+annotate(geom="text",x=0.95,
+         y=0.05,label="Treatment", color="black") +
