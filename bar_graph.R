910748c0b795a184512eba0a00fb4a0db0093f4d
Victoria M. Coutts <vzc0035@auburn.edu>
2020-04-15 18:21:08 -0500

    updated script so it generates a working bar graph

--- bar_graph.R
+++ bar_graph.R
@@ -5,7 +5,7 @@
 #different graph options.
 
 #first, designate where you want to save your graphs
-setwd("C:\Users\Tori Coutts\Desktop")
+setwd("/Users/Tori Coutts/Desktop")
 getwd()
 
 #upload the file containing the data you want to plot
@@ -16,21 +16,18 @@ datum=read.csv(file.choose())
 head(datum)
 
 #create a tiff file for your graph
-#tiff("MomVsEggMass.tiff",width=8,height=6,units='in',res=300)
+tiff("MomVsEggMass.tiff",width=8,height=6,units='in',res=300)
 
 #add the data and axes to the graph
 MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=AVGEGGMASS)) +
     geom_bar(stat="identity",fill="steelblue") +
     theme_minimal() +
     labs(title="", x="Mom ID", y="Average Egg Mass (g)")
+    theme(axis.title.x = element_text(size=10,vjust=0),
+          axis.text.x = element_text(size=8,color="black"),
+          axis.title.y = element_text(size=10,vjust=3),
+          axis.text.y = element_text(size=8,color="black"))
 MomVsEggMass
-#,data=datum,aes(x=MOM,y=EGGMASS,color="black") +
-#    theme_classic() +
-#    labs(title="",x="Mom ID",y="Egg Mass (g)") +
-#    theme(axis.title.x = element_text(size=10,vjust=0),
-#        axis.text.x = element_text(size=8,color="black"),
-#        axis.title.y = element_text(size=10,vjust=3),
-#        axis.text.y = element_text(size=8,color="black"))
 
 #turn the development of the tiff graph off
 dev.off()
