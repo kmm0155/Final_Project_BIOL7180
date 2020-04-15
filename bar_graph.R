45352f247879543aaca8477207eb1654ec58dac9
Victoria M. Coutts <vzc0035@auburn.edu>
2020-04-15 10:35:42 -0500

    added code

--- bar_graph.R
+++ bar_graph.R
@@ -8,10 +8,25 @@
 setwd("C:\Users\Tori Coutts\Desktop")
 getwd()
 
+#upload the file containing the data you want to plot
+#MUST be a csv file
+datum=read.csv(file.choose())
+
+#make sure the data was properly uploaded by seeing just a few rows
+head(datum)
+
 #create a tiff file for your graph
-tiff("EEF2.tiff",width=8,height=6,units='in',res=300)
+tiff("MomVsEggMass.tiff",width=8,height=6,units='in',res=300)
 
 #add the data and axes to the graph
+MomVsEggMass <- ggplot() +
+    geom_bar(stat="identity",data=datum,aes(x=MOM,y=EGGMASS,color="black") +
+    theme_classic() +
+    labs(title="",x="Mom ID",y="Egg Mass (g)") +
+    theme(axis.title.x = element_text(size=10,vjust=0),
+        axis.text.x = element_text(size=8,color="black"),
+        axis.title.y = element_text(size=10,vjust=3),
+        axis.text.y = element_text(size=8,color="black"))
 
 #turn the development of the tiff graph off
 dev.off()
