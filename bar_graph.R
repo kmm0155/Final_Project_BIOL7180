a60f814c0f530c48116ea85a1c94f6d68014c07b
Victoria M. Coutts <vzc0035@auburn.edu>
2020-04-15 14:22:06 -0500

    tried to add egg ID into separate bars to fix the graph

--- bar_graph.R
+++ bar_graph.R
@@ -19,9 +19,8 @@ head(datum)
 #tiff("MomVsEggMass.tiff",width=8,height=6,units='in',res=300)
 
 #add the data and axes to the graph
-MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=EGGMASS)) +
-    geom_bar(stat="identity",fill="steelblue") +
-    theme_minimal()
+MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=EGGMASS,fill=EGGID)) +
+    geom_bar(stat="identity",position=position_dodge())
 MomVsEggMass
 #,data=datum,aes(x=MOM,y=EGGMASS,color="black") +
 #    theme_classic() +
