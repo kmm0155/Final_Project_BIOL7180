194898220ff795d191fcb5a1df4444425099804f
Megan Roberts <mrr0019@auburn.edu>
2020-04-17 11:32:09 -0500

    Shebang line and Set working directory

--- GGplot_LineGraph_Script.R
+++ GGplot_LineGraph_Script.R
@@ -1,1 +1,9 @@
+#!/usr/bin/Rscript
+
 # This script will be used as a piece of the whole, but is for the creation of a 3-D Line Graph in GGplot within R.
+# This script is also set up to be ran individually if desired
+
+# Set working directory and where to save your graph
+setwd("/home/aubcls98/Final_Project_BIOL7180")
+getwd()
+
