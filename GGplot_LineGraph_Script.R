9520ba8172657e38437b3fbe33bc23a69d04633f
Megan Roberts <mrr0019@auburn.edu>
2020-04-18 15:38:30 -0500

    Fine tuning

--- GGplot_LineGraph_Script.R
+++ GGplot_LineGraph_Script.R
@@ -13,6 +13,9 @@ datum=read.csv(file.choose()))
 # View data to make sure it was uploaded correctly 
 head(datum)
 
+# Open GGplot
+library(ggplot2)
+
 # Using GGplot, add the data and axes to the graph
 # data= the variable name that you chose for your data (datum)
 # aes= the names of the columns that your data is in
@@ -23,7 +26,7 @@ Line_Graph <- ggplot() +
     theme_minimal() +
     labs(title="",x="Egg mass (g) at oviposition", y="Hatchling SVL (mm)") +
     scale_x_continuous(breaks=seq(0.85,1.25,0.05), limits=c(0.85,1.0)) +
-    theme(axis.title.x = element_test(size=10,vjust=0),
+    theme(axis.title.x = element_text(size=10,vjust=0),
     axis.title.x = element_text(size=8,color="black"),
     axis.title.y = element_text(size=10,vjust=3),
     axis.title.y = element_text(size=8,color="black"))
