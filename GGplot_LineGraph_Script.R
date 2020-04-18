9394d7075bc1245df7a381cf0463e059239e7a27
Megan Roberts <mrr0019@auburn.edu>
2020-04-18 12:45:10 -0500

    Fixed/commented graphing portion/added dev.off

--- GGplot_LineGraph_Script.R
+++ GGplot_LineGraph_Script.R
@@ -14,13 +14,19 @@ datum=read.csv(file.choose()))
 head(datum)
 
 # Using GGplot, add the data and axes to the graph
-Body_Condition <- ggplot() +
-    geom_line(data=datum2, aes(x=EGGMASS,y=resids)) +
+# data= the variable name that you chose for your data (datum)
+# aes= the names of the columns that your data is in
+# labs= where you title your graph and each axis
+# theme= where you specify your font sizes and color
+Line_Graph <- ggplot() +
+    geom_line(data=datum, aes(x=EGGMASS,y=SVL)) +
     theme_minimal() +
-    labs(title="",x="Egg mass (g) at oviposition", y="Hatchling body condition") +
+    labs(title="",x="Egg mass (g) at oviposition", y="Hatchling SVL (mm)") +
     scale_x_continuous(breaks=seq(0.85,1.25,0.05), limits=c(0.85,1.0)) +
     theme(axis.title.x = element_test(size=10,vjust=0),
     axis.title.x = element_text(size=8,color="black"),
     axis.title.y = element_text(size=10,vjust=3),
     axis.title.y = element_text(size=8,color="black"))
-    
+
+# Turn off the graphing device
+dev.off()
