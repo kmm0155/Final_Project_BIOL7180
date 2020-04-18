cb3e425ae44e4a80a9d36793de1996556e5f01c2
vzc0035 <60453535+vzc0035@users.noreply.github.com>
2020-04-18 18:53:12 -0500

    Update bar_graph.R

--- bar_graph.R
+++ bar_graph.R
@@ -17,7 +17,6 @@ head(datum)
 
 response = select.list(c("1-1", "1-2", "1-3", "2-1", "2-2", "2-3", "3-1", "3-2", "3-3"), preselect=NULL, multiple=FALSE)
 if(response == "1-1") {
-tiff("MomVsEggMass.tiff",width=8,height=6,units='in',res=300)
 MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=AVGEGGMASS)) +
     geom_bar(stat="identity",fill="steelblue") +
     theme_minimal() +
@@ -27,7 +26,7 @@ MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=AVGEGGMASS)) +
          axis.title.y = element_text(size=10,vjust=3),
          axis.text.y = element_text(size=8,color="black"))
 MomVsEggMass
-dev.off()
+ggsave(file="MomVsEggMass.tiff")
 }
 #create a tiff file for your graph
 #tiff("MomVsEggMass.tiff",width=8,height=6,units='in',res=300)
