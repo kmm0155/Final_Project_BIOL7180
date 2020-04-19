34f41cee898c97d32ea4cacd090cb36c3cb72b54
vzc0035 <60453535+vzc0035@users.noreply.github.com>
2020-04-18 19:07:36 -0500

    Update bar_graph.R

--- bar_graph.R
+++ bar_graph.R
@@ -15,7 +15,14 @@ datum=read.csv(file.choose())
 #make sure the data was properly uploaded by seeing just a few rows
 head(datum)
 
+#selecting which graph you would like
+cat("Choose which graph and file type you would like:
+	1-1: Bar graph, tiff
+	1-2: Bar graph, png
+	1-3: Bar graph, jpg \n")
 response = select.list(c("1-1", "1-2", "1-3", "2-1", "2-2", "2-3", "3-1", "3-2", "3-3"), preselect=NULL, multiple=FALSE)
+
+#1-1: bar graph saved as a tiff
 if(response == "1-1") {
 MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=AVGEGGMASS)) +
     geom_bar(stat="identity",fill="steelblue") +
@@ -28,20 +35,32 @@ MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=AVGEGGMASS)) +
 MomVsEggMass
 ggsave(file="MomVsEggMass.tiff")
 }
-#create a tiff file for your graph
-#tiff("MomVsEggMass.tiff",width=8,height=6,units='in',res=300)
 
-#add the data and axes to the graph
-#MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=AVGEGGMASS)) +
-#    geom_bar(stat="identity",fill="steelblue") +
-#    theme_minimal() +
-#    labs(title="", x="Mom ID", y="Average Egg Mass (g)")
-#    theme(axis.title.x = element_text(size=10,vjust=0),
-#          axis.text.x = element_text(size=8,color="black"),
-#          axis.title.y = element_text(size=10,vjust=3),
-#          axis.text.y = element_text(size=8,color="black"))
-#MomVsEggMass
+#1-2: bar graph saved as a png
+if(response == "1-2") {
+MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=AVGEGGMASS)) +
+    geom_bar(stat="identity",fill="steelblue") +
+    theme_minimal() +
+    labs(title="", x="Mom ID", y="Average Egg Mass (g)")
+    theme(axis.title.x = element_text(size=10,vjust=0),
+         axis.text.x = element_text(size=8,color="black"),
+         axis.title.y = element_text(size=10,vjust=3),
+         axis.text.y = element_text(size=8,color="black"))
+MomVsEggMass
+ggsave(file="MomVsEggMass.png")
+}
 
-#turn the development of the tiff graph off
-#dev.off()
+#1-3: bar graph saved as a jpg
+if(response == "1-3") {
+MomVsEggMass <- ggplot(data=datum,aes(x=MOM,y=AVGEGGMASS)) +
+    geom_bar(stat="identity",fill="steelblue") +
+    theme_minimal() +
+    labs(title="", x="Mom ID", y="Average Egg Mass (g)")
+    theme(axis.title.x = element_text(size=10,vjust=0),
+         axis.text.x = element_text(size=8,color="black"),
+         axis.title.y = element_text(size=10,vjust=3),
+         axis.text.y = element_text(size=8,color="black"))
+MomVsEggMass
+ggsave(file="MomVsEggMass.jpg")
+}
 
