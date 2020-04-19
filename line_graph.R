a263f644c1dcc3ae429b5cf25a598193a8d11d1f
vzc0035 <60453535+vzc0035@users.noreply.github.com>
2020-04-19 15:58:32 -0500

    Update line_graph.R

--- line_graph.R
+++ line_graph.R
@@ -1,13 +1,5 @@
-#!/usr/bin/bash
-
 # This script will be used as a piece of the whole, but is for the creation of a 3-D Line Graph in GGplot within R.
 # This script is also set up to be ran individually if desired
-
-# Starting up R
-source /opt/asn/etc/asn-bash-profiles-special/modules.sh
-module load R/3.6.1
-R CMD BATCH my_r_program.r
-R
 
 # Set working directory and where to save your graph
 setwd("/home/aubcls98/Final_Project_BIOL7180")
@@ -43,47 +35,32 @@ response2 = select.list(c("3-1", "3-2", "3-3"), preselect=NULL, multiple=FALSE)
 
 # First graph selection is 3-1- line graph with tiff output
 if (response2 == "3-1") {
-Line_Graph <- ggplot(data=datum,aes(x=EGGMASS,y=SVL)) +
-    geom_line(stat="identity") +
+Line_Graph = ggplot() +
+    geom_line(data=datum, aes(x=EGGMASS,y=SVL)) +
     theme_minimal() +
     labs(title="",x="Egg mass (g) at oviposition", y="Hatchling SVL (mm)") +
     scale_x_continuous(breaks=seq(0.85,1.25,0.05), limits=c(0.85,1.0)) +
-    theme(axis.title.x = element_text(size=10,vjust=0),
-    axis.title.x = element_text(size=8,color="black"),
-    axis.title.y = element_text(size=10,vjust=3),
-    axis.title.y = element_text(size=8,color="black"))
-Line_Graph
-ggsave(file="Line_Graph.tiff")
+ggsave(file="line_graph.tiff")
 }
 
 # Second graph selection is 3-2- line graph with png output
 if (response2 == "3-2") {
-Line_Graph <- ggplot(data=datum,aes(x=EGGMASS,y=SVL)) +
-    geom_line(stat="identity") +
+Line_Graph = ggplot() +
+    geom_line(data=datum, aes(x=EGGMASS,y=SVL)) +
     theme_minimal() +
     labs(title="",x="Egg mass (g) at oviposition", y="Hatchling SVL (mm)") +
     scale_x_continuous(breaks=seq(0.85,1.25,0.05), limits=c(0.85,1.0)) +
-    theme(axis.title.x = element_text(size=10,vjust=0),
-    axis.title.x = element_text(size=8,color="black"),
-    axis.title.y = element_text(size=10,vjust=3),
-    axis.title.y = element_text(size=8,color="black"))
-Line_Graph
-ggsave(file="Line_Graph.png")
+ggsave(file="line_graph.png")
 }
 
 # Third graph selection is 3-3- line graph with jpeg output
 if (response2 == "3-3") {
-Line_Graph <- ggplot(data=datum,aes(x=EGGMASS,y=SVL)) +
-    geom_line(stat="identity") +
+Line_Graph = ggplot() +
+    geom_line(data=datum, aes(x=EGGMASS,y=SVL)) +
     theme_minimal() +
     labs(title="",x="Egg mass (g) at oviposition", y="Hatchling SVL (mm)") +
     scale_x_continuous(breaks=seq(0.85,1.25,0.05), limits=c(0.85,1.0)) +
-    theme(axis.title.x = element_text(size=10,vjust=0),
-    axis.title.x = element_text(size=8,color="black"),
-    axis.title.y = element_text(size=10,vjust=3),
-    axis.title.y = element_text(size=8,color="black"))
-Line_Graph
-ggsave(file="Line_Graph.jpeg")
+ggsave(file="line_graph.jpg")
 }
 
 # Turn off the graphing device
