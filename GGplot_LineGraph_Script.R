d7d467e6791a396ff2442c40c8be2433b44252a3
Megan Roberts <mrr0019@auburn.edu>
2020-04-18 20:16:23 -0500

    Added tiff, png, and jpeg output options

--- GGplot_LineGraph_Script.R
+++ GGplot_LineGraph_Script.R
@@ -29,6 +29,7 @@ response = select.list(c("3-1", "3-2", "3-3"), preselect=NULL, multiple=FALSE)
 # aes= the names of the columns that your data is in
 # labs= where you title your graph and each axis
 # theme= where you specify your font sizes and color
+
 # First graph selection is 3-1- line graph with tiff output
 if (response == "3-1") {
 Line_Graph <- ggplot(data=datum,aes(x=EGGMASS,y=SVL)) +
@@ -43,6 +44,37 @@ Line_Graph <- ggplot(data=datum,aes(x=EGGMASS,y=SVL)) +
 Line_Graph
 ggsave(file="Line_Graph.tiff")
 }
+
+# Second graph selection is 3-2- line graph with png output
+if (response == "3-2") {
+Line_Graph <- ggplot(data=datum,aes(x=EGGMASS,y=SVL)) +
+    geom_line(stat="identity") +
+    theme_minimal() +
+    labs(title="",x="Egg mass (g) at oviposition", y="Hatchling SVL (mm)") +
+    scale_x_continuous(breaks=seq(0.85,1.25,0.05), limits=c(0.85,1.0)) +
+    theme(axis.title.x = element_text(size=10,vjust=0),
+    axis.title.x = element_text(size=8,color="black"),
+    axis.title.y = element_text(size=10,vjust=3),
+    axis.title.y = element_text(size=8,color="black"))
+Line_Graph
+ggsave(file="Line_Graph.png")
+}
+
+# Third graph selection is 3-3- line graph with jpeg output
+if (response == "3-3") {
+Line_Graph <- ggplot(data=datum,aes(x=EGGMASS,y=SVL)) +
+    geom_line(stat="identity") +
+    theme_minimal() +
+    labs(title="",x="Egg mass (g) at oviposition", y="Hatchling SVL (mm)") +
+    scale_x_continuous(breaks=seq(0.85,1.25,0.05), limits=c(0.85,1.0)) +
+    theme(axis.title.x = element_text(size=10,vjust=0),
+    axis.title.x = element_text(size=8,color="black"),
+    axis.title.y = element_text(size=10,vjust=3),
+    axis.title.y = element_text(size=8,color="black"))
+Line_Graph
+ggsave(file="Line_Graph.jpeg")
+}
+
 
 # Turn off the graphing device
 dev.off()
